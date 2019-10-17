78ce036be93edf1126c356a84aab92cc1b088e72
liyy <ants_double@yeah.net>
2019-10-17 17:23:28 +0800

    electron c# project

---
+++ electronedge/electronedge/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace electronedge
+{
+    public class Class1
+    {
+        public async Task<Object> Invoke(object input)
+        {
+            return Helper.SayHi("Invoke1:" + (string)input);
+        }
+
+        public async Task<Object> Invoke2(object input)
+        {
+            return  Helper.SayHi("Invoke2:" + (string)input);
+        }
+
+        static class Helper
+        {
+            public static string SayHi(string param)
+            {
+                return ".NET Welcomes " + param;
+            }
+        }
+    }
+}
